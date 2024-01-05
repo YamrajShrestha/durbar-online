59788e57f1bc15d8d9a99ead034432ab26a4bfed
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-01-05 11:57:09 +0545

    components, pages set up

---
+++ client/src/Components/Top.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Top = () => {
+  return (
+    <>
+      <section className="top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <ul className="d-flex justify-content-end gap-4">
+                <li>
+                  <a href="">Become a Seller !</a>
+                </li>
+                <li>
+                  <a href="">Login to Seller</a>
+                </li>
+                <li>
+                  <a href="">Helpline 9863667252 | 9801876930</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Top;
